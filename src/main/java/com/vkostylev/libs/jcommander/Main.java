37a621d36bab55358bf80c9e1b265988e82bc10e
kostylevv <kostylev.v@gmail.com>
2022-05-31 18:18:11 +0300

    Jcommander example modified

--- src/main/java/com/vkostylev/libs/jcommander/Main.java
+++ src/main/java/com/vkostylev/libs/jcommander/Main.java
@@ -9,24 +9,33 @@ public class Main {
     @Parameter
     private List<String> parameters = new ArrayList<>();
 
-    @Parameter(names = { "-log", "-verbose" }, description = "Level of verbosity")
-    private Integer verbose = 1;
+    @Parameter(names = { "-t", "-type" }, description = "Type of command")
+    private String type;
 
-    @Parameter(names = "-groups", description = "Comma-separated list of group names to be run")
-    private String groups;
+    @Parameter(names = "-i", description = "Index in storage")
+    private int index = -1;
 
-    @Parameter(names = "-debug", description = "Debug mode")
-    private boolean debug = false;
+    @Parameter(names = "-m", description = "Message to store", variableArity = true)
+    private List<String> message;
 
     public static void main(String[] args) {
+        String words = "-t set -i 148 -m Here is some text to store on the server";
         Main mArgs = new Main();
-        String[] argv = { "-log", "2", "-groups", "unit" };
+        //String[] argv = { "-log", "2", "-groups", "unit" };
+        String[] argc = words.split(" ");
         JCommander.newBuilder()
                 .addObject(mArgs)
                 .build()
-                .parse(argv);
+                .parse(argc);
 
-        System.out.println(mArgs.groups);
+        System.out.println(mArgs.type);
+        System.out.println(mArgs.index);
+        System.out.println(mArgs.message);
+        StringBuilder msg = new StringBuilder();
+        for (String word : mArgs.message) {
+            msg.append(word + " ");
+        }
+        System.out.println(msg.toString());
 
     }
 }
